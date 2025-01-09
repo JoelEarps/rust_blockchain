8710942d09723ab1a2c2cd829024ba8f136d3bc2
JoelEarps <joeldearps@gmail.com>
2025-01-09 10:00:42 +0000

    chore: add pseudo code for future implementation

---
+++ src/consensus_mechanisms/proof_of_work/mining.rs
@@ -0,0 +1,16 @@
+/*
+Function starts mining and returns
+Handles any errors - can there be errors just an invalid or valid proof?
+Returns valid proof
+Does this handle trigger a new block and adding it to the block chain, or should this be injected into the blockchain class and called
+What makes for better encapsulation */
+fn start_mining(){
+    perform_mathematical_proof();
+    todo!()
+}
+
+// This function is responsible for handling the mathematical
+// TODO: Come up with example algorithm
+fn perform_mathematical_proof(){
+    todo!()
+}
